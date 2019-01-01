de659393356232d2ae228ab64fc5e02a488c5b38
Sarah <sarah.benfdil@gmail.com>
2019-01-01 14:42:40 +0100

    app.js

--- app.js
+++ app.js
@@ -8,11 +8,20 @@ const
   body_parser = require('body-parser'),
   app = express().use(body_parser.json()); // creates express http server
 
-// Sets server port and logs message on success
-app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+  app.set('port', (process.env.PORT || 5000))
 
-// Accepts GET requests at the /webhook endpoint
-app.get('/webhook', (req, res) => {
+  // Process application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({extended: false}))
+
+  // Process application/json
+  app.use(bodyParser.json())
+
+  // Index route
+  app.get('/', function (req, res) {
+      res.send('Hello world, I am a chat bot')
+  });
+  // for Facebook verification
+app.get('/webhook/', function (req, res) {
 
   /** UPDATE YOUR VERIFY TOKEN **/
   const VERIFY_TOKEN = "token_test";
@@ -21,23 +30,22 @@ app.get('/webhook', (req, res) => {
   let mode = req.query['hub.mode'];
   let token = req.query['hub.verify_token'];
   let challenge = req.query['hub.challenge'];
-
-  // Check if a token and mode were sent
-  if (mode && token) {
-
     // Check the mode and token sent are correct
     if (mode /*=== 'subscribe'*/ && token === VERIFY_TOKEN) {
 
       // Respond with 200 OK and challenge token from the request
       console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
+      res.send(challenge);
 
     } else {
       // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);
+      res.send('No entry');
     }
-//   }
-// });
+  });
+  // Spin up the server
+  app.listen(app.get('port'), function() {
+      console.log('running on port', app.get('port'))
+  })
 // // Accepts POST requests at /webhook endpoint
 // app.post('/', (req, res) => {
 //
